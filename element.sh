37fd91955f3e919bee8e7405821b5a285a7c3dd2
Moustapha <amroune.moustapha@gmail.com>
2025-08-21 06:31:37 -0400

    chore: update comments in element.sh

--- element.sh
+++ element.sh
@@ -2,35 +2,35 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# No argument, then exit
+# Check if an argument is provided
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
   exit
 fi
 
-# 
+# Determine if input is atomic number (digits) or symbol/name
 if [[ $1 =~ ^[0-9]+$ ]]; then
   CONDITION="e.atomic_number = $1"
 else
   CONDITION="e.symbol = '$1' OR e.name = '$1'"
 fi
 
-# GET INFOS
+# Query database for element information
 RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
 FROM elements e
 JOIN properties p USING(atomic_number)
 JOIN types t USING(type_id)
 WHERE $CONDITION;")
 
-# IS ELEMENT DOESN't EXIST
+#  If element is not found
 if [[ -z $RESULT ]]
 then
   echo "I could not find that element in the database."
   exit
 fi
 
-# DISPLAY
+# Format and display result
 IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
 
